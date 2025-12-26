c83f504a074f9f1ac29fee2cb1025515b7a45a07
dev-protim <roypranto445@gmail.com>
2025-12-26 03:39:23 +0100

    Fixed paddle and ball movement.

--- Scripts/Ball/ball.gd
+++ Scripts/Ball/ball.gd
@@ -1,10 +1,11 @@
 extends RigidBody3D
 
 @export var speed: float = 10.0
-var direction := Vector3(1, 1, 0).normalized()
 
 func _ready():
-	linear_velocity = direction * speed
+	var dir = Vector3(-1, 1, 0).normalized()
+	linear_velocity = dir * speed
+	gravity_scale = 0
 
 func _physics_process(delta):
 	linear_velocity = linear_velocity.normalized() * speed
